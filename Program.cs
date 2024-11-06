a3295e68d71486ea8d39ddba93188b1933301887
ReoF777 <u035340j@ecs.osaka-u.ac.jp>
2024-11-06 15:03:16 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenCvSharp;
+// 使用するWebカメラを選択するための試し用コード
+using(var capture = new VideoCapture(0, VideoCaptureAPIs.DSHOW)) // カメラから画像を取得し、連続的に表示するプログラム
+{
+    using (var window = new Window("capture"))
+    {
+        Mat image = new Mat();
+        while (true)
+        {
+            capture.Read(image);
+            window.ShowImage(image);
+            Cv2.WaitKey(1);
+        }
+    }
+}
